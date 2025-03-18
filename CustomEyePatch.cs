b480d56abf25e9f091b49c1c615d55c399d756bf
s1ckboy <sickxedits@gmail.com>
2025-03-19 00:58:10 +0100

    Add project files.

---
+++ CustomEyePatch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+
+public class CustomEyePatch : MonoBehaviour
+{
+    public GameObject LeftPupilObject { get; private set; }
+    public GameObject RightPupilObject { get; private set; }
+
+    public CustomEyePatch(GameObject leftPupilObject, GameObject rightPupilObject)
+    {
+        LeftPupilObject = leftPupilObject;
+        RightPupilObject = rightPupilObject;
+    }
+
+    public void ReplaceEyePatches(GameObject newLeftEye, GameObject newRightEye)
+    {
+        // Destroy the existing eye patches
+        Destroy(LeftPupilObject);
+        Destroy(RightPupilObject);
+
+        // Instantiate the new eye patches
+        LeftPupilObject = Instantiate(newLeftEye, LeftPupilObject.transform.parent);
+        RightPupilObject = Instantiate(newRightEye, RightPupilObject.transform.parent);
+    }
+
+    private void Destroy(GameObject gameObject)
+    {
+        UnityEngine.Object.Destroy(gameObject);
+    }
+}
---
+++ MoreEyes.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using BepInEx;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MoreEyes
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    [HarmonyPatch(typeof(PlayerAvatar), "Spawn")]
+    public class PlayerSpawnPatch : BaseUnityPlugin
+    {
+        private static List<CustomEyePatch> customEyePatches = [];
+        public void Awake()
+        {
+            string pluginFolderPath = Path.GetDirectoryName(Info.Location);
+            string assetBundleFilePath = Path.Combine(pluginFolderPath, "eyes");
+            AssetBundle assetBundle = AssetBundle.LoadFromFile(assetBundleFilePath);
+            GameObject cat_iris = assetBundle.LoadAsset<GameObject>("cat_iris");
+            CustomEyePatch catEyePatch = new(cat_iris, cat_iris);
+            customEyePatches.Add(catEyePatch);
+
+        }
+        public static void Postfix(PlayerAvatar __instance)
+        {
+            //make sure we are an actual player and not in the menus
+            if (SemiFunc.RunIsLobbyMenu() || RunManager.instance.levelCurrent == RunManager.instance.levelMainMenu)
+                return;
+
+            List<PlayerAvatar> allPlayers = SemiFunc.PlayerGetAll();
+
+            allPlayers.Do(p => GetPlayerEyes(p));
+        }
+
+        internal static void GetPlayerEyes(PlayerAvatar player)
+        {
+            Transform pupilLeft = player.playerAvatarVisuals.playerEyes.pupilLeft;
+            Transform pupilRight = player.playerAvatarVisuals.playerEyes.pupilRight;
+
+            if (pupilLeft.childCount == 0 || pupilRight.childCount == 0) //throw an error that pupil could not be found
+                return;
+
+            GameObject leftPupilObject = pupilLeft.GetChild(0).gameObject;
+            GameObject rightPupilObject = pupilRight.GetChild(0).gameObject;
+
+            foreach (CustomEyePatch customEyePatch in customEyePatches)
+            {
+                customEyePatch.ReplaceEyePatches(customEyePatch.LeftPupilObject, customEyePatch.RightPupilObject);
+            }
+        }
+    }
+}
