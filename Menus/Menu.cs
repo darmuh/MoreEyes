a02b68f10c114620c55bfc4a3a19eb4ea1a86b11
s1ckboy <sickxedits@gmail.com>
2025-03-25 10:29:01 +0100

    placerholder placerholder placeholder

--- Menus/Menu.cs
+++ Menus/Menu.cs
@@ -1,21 +1,38 @@
 ﻿using UnityEngine;
 using BepInEx;
 using MenuLib;
+using MenuLib.MonoBehaviors;
+using Unity.VisualScripting;
+using System.Collections.Generic;
 
 namespace MoreEyes.Menus;
 
 internal sealed class Menu
 {
+    internal static REPOButton clickedButton;
+
+    private static readonly List<GameObject> placeholder = [];
 
     internal static void Initialize()
     {
-        MenuAPI.AddElementToMainMenu(parent => MenuAPI.CreateREPOButton("MoreEyes", CreateElement, parent, new Vector2(48.3f, 55.5f))); // Change these later
-        MenuAPI.AddElementToLobbyMenu(parent => MenuAPI.CreateREPOButton("MoreEyes", CreateElement, parent, new Vector2(186f, 32)));
-        MenuAPI.AddElementToEscapeMenu(parent => MenuAPI.CreateREPOButton("MoreEyes", CreateElement, parent, new Vector2(126f, 86f)));
+        MenuAPI.AddElementToMainMenu(p => MenuAPI.CreateREPOButton("MoreEyes", CreateElement, p, new Vector2(48.3f, 55.5f))); // Change these later
+        MenuAPI.AddElementToLobbyMenu(p => MenuAPI.CreateREPOButton("MoreEyes", CreateElement, p, new Vector2(186f, 32)));
+        MenuAPI.AddElementToEscapeMenu(p => MenuAPI.CreateREPOButton("MoreEyes", CreateElement, p, new Vector2(126f, 86f)));
     }
     private static void CreateElement()
     {
-        // Open popup with MenuAPI.CreateREPOPopupPage()
-        // Create a back button for this popup page with MENUAPI.CreateREPOButton()
+        clickedButton = null;
+
+        var REPOPopup = MenuAPI.CreateREPOPopupPage("Mods", REPOPopupPage.PresetSide.Left, false, true);
+
+        REPOPopup.AddElement(p => MenuAPI.CreateREPOButton("Back", () => {
+            if (placeholder.Count == 0)
+            {
+                REPOPopup.ClosePage(true);
+                return;
+            }
+        }, p, new Vector2(66f, 18f)));
+
+        // Totally placeholder
     }
 }
